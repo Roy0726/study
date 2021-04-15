2e314b237c096c71d3a29ccc0d2eb308af95c0e8
commonsoft <commonsoftcs@gmail.com>
2021-04-15 14:25:52 +0900

    12

--- 300_1.py
+++ 300_1.py
@@ -1,6 +1,32 @@
-#1
+# 1
 print("Hello World")
-#2
+# 2
 print("Mary's cosmetics")
-#3
+# 3
 print("신씨가 소리질렀다. \"도둑이야\"")
+# 4
+print("\"C:\\Windows\"")
+# 5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+# 6
+print("오늘은", "일요일")
+# 7
+print("naver;kakao;sk;samsung")
+# 8
+print("naver/kakao/sk/samsung")
+# 9
+print("first", end="")
+print("second")
+# 10
+print(5/3)
+# 11
+samsung = 50000
+total_value = samsung*10
+print(total_value, "원")
+# 12
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
